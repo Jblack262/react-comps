29c16d53d1282b67f8ccc6ec0176cfc80e724f43
Jblack262 <JBlack262@west-mec.org>
2021-09-06 23:13:24 -0700

    fixed accordion menu link errors

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -6,25 +6,25 @@ function Accordion() {
             <li className="item" id="profile">
                 <a href="#profile" className="btn">Profile</a>
                 <div className="smenu">
-                    <a href="#">a</a>
-                    <a href="#">b</a>
-                    <a href="#">c</a>
+                    <a href="/">a</a>
+                    <a href="/">b</a>
+                    <a href="/">c</a>
                 </div>
             </li>
             <li className="item" id='messages'>
                 <a href="#messages" className="btn">Messages</a>
                 <div className="smenu">
-                    <a href="#">a</a>
-                    <a href="#">b</a>
-                    <a href="#">c</a>
+                    <a href="/">a</a>
+                    <a href="/">b</a>
+                    <a href="/">c</a>
                 </div>
             </li>
             <li className="item" id='settings'>
                 <a href="#settings" className="btn">Settings</a>
                 <div className="smenu">
-                    <a href="#">a</a>
-                    <a href="#">b</a>
-                    <a href="#">c</a>
+                    <a href="/">a</a>
+                    <a href="/">b</a>
+                    <a href="/">c</a>
                 </div>
             </li>
         </ul>
